93499cec61daf50d06fea350bf05dc6451255678
Vinay Arjun Hajare <106072427+VinayHajare@users.noreply.github.com>
2025-01-07 17:28:57 +0530

    Delete Music-Streaming-API directory

--- Music-Streaming-API/src/main/java/com/musicstreamingapi/repository/PlaylistSongRepository.java
+++
@@ -1,9 +0,0 @@
-package com.musicstreamingapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.musicstreamingapi.model.PlaylistSong;
-
-public interface PlaylistSongRepository extends JpaRepository<PlaylistSong, Long>{
-
-}
